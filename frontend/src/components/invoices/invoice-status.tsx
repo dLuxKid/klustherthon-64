4bea69f88c72446707871e2a9dd2f614e77daa37
dLuxKid <adetunjimarvellous09@gmail.com>
2023-11-25 11:18:57 +0100

    ui changes

--- frontend/src/components/invoices/invoice-status.tsx
+++ frontend/src/components/invoices/invoice-status.tsx
@@ -5,8 +5,8 @@ export default function InvoiceStatus({ status }: { status: string }) {
     return (
         <span
             className={` inline-flex items-center rounded-full px-2 py-1 text-xs
-             ${status === 'pending' && 'bg-gray-100 text-gray-500'} 
-            ${status === 'paid' && 'bg-green-500 text-white'}
+             ${status === 'pending' && 'bg-gray-100 text-text'} 
+            ${status === 'paid' && 'bg-success text-white'}
       `}
         >
             {status === 'pending' ? (
