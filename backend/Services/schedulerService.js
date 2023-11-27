e5d5e10515c27478152f964f5d50cc975cda0bc2
DamiAdeeso <damilolaadeeso@gmail.com>
2023-11-27 22:45:38 +0100

    almost done with scheduler

---
+++ backend/Services/schedulerService.js
@@ -0,0 +1,15 @@
+import schedule from "node-schedule";
+import {
+    sendMail
+} from "./EmailService.js";
+
+
+
+
+export const scheduleInvoice = (date,emailAddresses,businessName,amount,dueDate,title,clientName) => {
+    const job = schedule.scheduleJob(date, () => {
+        sendMail(emailAddresses, businessName,
+          amount, dueDate, title, clientName
+        ).catch(console.error)
+    });
+}
