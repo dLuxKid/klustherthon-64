4550c8e065aeed2bfd1d116a75c910d6a6722177
dLuxKid <adetunjimarvellous09@gmail.com>
2023-11-28 09:48:33 +0100

    ...

--- backend/Services/schedulerService.js
+++ backend/Services/schedulerService.js
@@ -1,10 +1,5 @@
 import schedule from "node-schedule";
-import {
-    sendMail
-} from "./EmailService.js";
-
-
-
+import { sendMail } from "./EmailService.js";
 
 export const scheduleInvoice = (
   date,
