ed13ff053ef5d9ed1cb844b8623871d0d88b10fd
dLuxKid <adetunjimarvellous09@gmail.com>
2023-11-28 12:55:36 +0100

    improved form validation

--- frontend/src/components/forms/staff-login.tsx
+++ frontend/src/components/forms/staff-login.tsx
@@ -1,4 +1,4 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import * as Yup from "yup";
@@ -21,8 +21,12 @@ const StaffLoginForm = ({ loginDetails }: Props) => {
     const { dispatch } = useAuthContext()
 
     const validationSchema = Yup.object({
-        email: Yup.string().required(),
-        password: Yup.string().min(8).max(20).required(),
+        email: Yup.string().required('Staff email is required'),
+        password: Yup.string()
+            .min(8, "Password must be at least 8 characters")
+            .max(20, "Password must be at most 20 characters")
+            .matches(/^(?=.*[a-zA-Z])(?=.*\d).{8,20}$/, "Password must contain letters and numbers")
+            .required("Password is required"),
     });
 
     const input = 'w-full px-4 h-[45px] rounded outline-none border-none text-text text-base font-normal'
@@ -81,25 +85,32 @@ const StaffLoginForm = ({ loginDetails }: Props) => {
             >
                 {({ isSubmitting, values }) => (
                     <Form className="flex items-stretch justify-center flex-col gap-4">
-                        <Field
-                            type="text"
-                            name="email"
-                            className={input}
-                            placeholder="Staff Email"
-                            required
-                        />
-                        <Field
-                            type="password"
-                            name="password"
-                            className={input}
-                            placeholder="Staff Password"
-                            required
-                        />
+                        <div>
+                            <p className="text-black font-medium mb-1">Staff Email</p>
+                            <Field
+                                type="text"
+                                name="email"
+                                className={input}
+                                placeholder="Staff Email"
+                                required
+                            />
+                            <ErrorMessage name="email" component='p' className="text-red-600 font-medium" />
+                        </div>
+                        <div>
+                            <p className="text-black font-medium mb-1">Staff Password</p>
+                            <Field
+                                type="password"
+                                name="password"
+                                className={input}
+                                placeholder="Staff Password"
+                                required
+                            />
+                            <ErrorMessage name="password" component='p' className="text-red-600 font-medium" />
+                        </div>
                         <button
                             type="submit"
                             className="w-full flex items-center justify-center bg-primary hover:bg-opacity-90 text-white font-semibold text-lg px-9 py-3 rounded-lg mt-4"
                             disabled={isSubmitting || !values.email || !values.password}
-
                         >
                             {isSubmitting ? <Loader /> : 'Login'}
                         </button>
