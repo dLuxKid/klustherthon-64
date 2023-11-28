75a4021ae2b21b39f88c0634867c409bbdff4a23
DamiAdeeso <damilolaadeeso@gmail.com>
2023-11-28 10:26:10 +0100

    fixing errors

--- backend/Services/emailService.js
+++ backend/Services/emailService.js
@@ -1,19 +1,10 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-dotenv.config();
-var smtpConfig = {
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // use SSL
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSKEY,
-  },
-};
-var transporter = nodemailer.createTransport(smtpConfig);
 
-// async..await is not allowed in global scope, must use a wrapper
-export async function sendMail(
+dotenv.config();
+
+// Use the 'await' keyword inside an async function
+async function sendMail(
   emailAddresses,
   businessName,
   paymentAmount,
@@ -21,24 +12,40 @@ export async function sendMail(
   title,
   userName
 ) {
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: `"${businessName}" <foo@example.com>`, // sender address
-    to: emailAddresses, // list of receivers
-    subject: "Invoice for " + title, // Subject line
-    text: `Hello ${userName}`, // plain text body
-    html: `<b>Hello ${userName}, \nYour Payment for ${title} of ${paymentAmount} is due on ${dueDate}</b>`, // html body
-  });
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  try {
+    var smtpConfig = {
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // use SSL
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSKEY,
+      },
+    };
 
-  //
-  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-  //       <https://github.com/forwardemail/preview-email>
-  //
+    var transporter = nodemailer.createTransport(smtpConfig);
+
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: `"${businessName}" <${process.env.EMAIL}>`, // use the configured email address as the sender
+      to: emailAddresses, // list of receivers
+      subject: "Invoice for " + title, // Subject line
+      text: `Hello ${userName},\nYour payment for ${title} of ${paymentAmount} is due on ${dueDate}`, // plain text body
+      html: `<b>Hello ${userName},<br>Your payment for ${title} of ${paymentAmount} is due on ${dueDate}</b>`, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 }
 
-// sendMail("damilolaadeeso@gmail.com, baz@example.com","Hair lock Factory",
-//     5000,23-12-2023,"Hairlocks","Geremy"
-// ).catch(console.error);
+// Example usage:
+// sendMail(
+//   "damilolaadeeso@gmail.com, baz@example.com",
+//   "Hair Lock Factory",
+//   5000,
+//   "23-12-2023",
+//   "Hairlocks",
+//   "Geremy"
+// );
--- backend/Services/schedulerService.js
+++ backend/Services/schedulerService.js
@@ -1,5 +1,5 @@
 import schedule from "node-schedule";
-import { sendMail } from "./EmailService.js";
+import { sendMail } from "./emailService.js";
 
 export const scheduleInvoice = (
   date,
