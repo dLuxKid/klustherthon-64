fd914a7500ea2078273de1385ac0a28089b07ffd
Adetunji Marvellous <adetunjimarvellous09@gmail.com>
2024-12-27 16:13:36 +0100

    Update urls.ts

--- frontend/src/utils/urls.ts
+++ frontend/src/utils/urls.ts
@@ -1,4 +1,4 @@
-export const usersUrl = "https://easy-ledger.onrender.com/api/users";
-export const invoiceUrl = "https://easy-ledger.onrender.com/api/invoices";
-export const paymentsUrl = "https://easy-ledger.onrender.com/api/payments";
-export const clientUrl = "https://easy-ledger.onrender.com/api/clients";
+export const usersUrl = "https://easy-ledger-new.onrender.com/api/users";
+export const invoiceUrl = "https://easy-ledger-new.onrender.com/api/invoices";
+export const paymentsUrl = "https://easy-ledger-new.onrender.com/api/payments";
+export const clientUrl = "https://easy-ledger-new.onrender.com/api/clients";
